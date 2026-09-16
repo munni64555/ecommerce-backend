cbbab7c8a286dd82691959fedd4621bcb944ac7b
Munni <kmunni6455@gmail.com>
2026-09-16 09:28:45 +0530

    Fix user controller import

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,35 +5,20 @@ import {
   loginUser,
   forgotPassword,
   verifyOtp,
-} from "../controllers/UserController.js";
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-
 // ================= Register =================
-
 router.post("/register", registerUser);
 
-
 // ================= Login =================
-
 router.post("/login", loginUser);
 
-
 // ================= Forgot Password - Send OTP =================
-
-router.post(
-  "/forgot-password",
-  forgotPassword
-);
-
+router.post("/forgot-password", forgotPassword);
 
 // ================= Verify OTP =================
-
-router.post(
-  "/verify-otp",
-  verifyOtp
-);
-
+router.post("/verify-otp", verifyOtp);
 
 export default router;
